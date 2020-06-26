14ef37038b0b876247157e4a120f244e52dbef7c
chm <chmielciu@gmail.com>
2020-06-26 02:08:45 +0200

    URL handling. Dummy xterm themes and dummy command palette

---
+++ components/palette/index.js
@@ -0,0 +1,20 @@
+const CmdPalette = require( 'electron-command-palette' );
+const functions = require( './functions' );
+let palette = new CmdPalette();
+const cmds = require( './commands.json' );
+
+//JSON style
+palette.add( cmds );
+
+//inline style
+palette.add(
+    { title: 'New project', category: 'Project', description: 'Create a new project from scratch', shortcut: 'CmdOrCtrl+Shift+N', action: 'newproject' }
+);
+
+//Module style
+palette.register( functions );
+
+//Inline style
+palette.register( 'saveproject', function() {
+    Project.save();
+} );
--- configs/config.js
+++ configs/config.js
@@ -1,33 +1,38 @@
 module.exports = {
     xterm: {
         terminal: {
-            fontFamily: 'Consolas NF',
-            fontSize: 14,
-            fontWeight: 'normal',
-            windowsMode: true,
-            cols: 120
+            fontFamily                : 'Consolas NF',
+            fontSize                  : 15,
+            fontWeight                : 'normal',
+            windowsMode               : true,
+            cols                      : 80,
+            drawBoldTextInBrightColors: true,
+            fontWeightBold            : 'normal',
+            allowTransparency         : true
         },
         profiles: [
             {
-                name: 'Wsl',
-                shell: 'wsl.exe'
+                name : 'nushell',
+                shell: 'C:/Utils/SHELL/nushell_0_15_0/nu.exe'
             }, {
-                name: 'cygwin',
-                shell: 'D:/cygwin64/bin/zsh.exe',
+                name : 'elvish',
+                shell: 'C:/Utils/SHELL/elvish.exe'
+            }, {
+                name     : 'cygwin',
+                shell    : 'D:/cygwin64/bin/zsh.exe',
                 shellArgs: ['-i']
             }, {
-                name: 'msys2',
-                shell: 'D:/msys64/usr/bin/zsh.exe',
+                name     : 'msys2',
+                shell    : 'D:/msys64/usr/bin/zsh.exe',
                 shellArgs: ['-i'],
-                env: Object.assign(this, process.env, { HOME: '/home/chm' })
+                env      : Object.assign( this, process.env, { HOME: '/home/chm' } )
             }, {
-                name: 'cmder',
-                shell: 'cmd.exe',
+                name     : 'cmder',
+                shell    : 'cmd.exe',
                 shellArgs: ['/k', 'C:/Utils/cmder/vendor/init.bat', '/f']
-
             }, {
-                name: 'cmder&buildTools',
-                shell: 'cmd.exe',
+                name     : 'cmder&buildTools',
+                shell    : 'cmd.exe',
                 shellArgs: ['/k', 'C:/Utils/cmder/vendor/init.bat', '/f', '&&', 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Auxiliary/Build/vcvars64.bat']
             }
         ]
