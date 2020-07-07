c60a3677a2814e0e53035c91d23c505483ee0e9e
chm <chmielciu@gmail.com>
2020-07-08 00:34:02 +0200

    Working react components - fm+command palette. no babel no jsx (htm used)

--- components/filemanager/index.js
+++ components/filemanager/index.js
@@ -1,30 +1,56 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-var createReactClass = require('create-react-class');
+
+const htm = require('htm');
+const html = htm.bind(React.createElement)
+
 const {FileManager, FileNavigator} = require('@opuscapita/react-filemanager');
-const connectorNodeV1 = require('@opuscapita/react-filemanager-connector-node-v1');
+const {default: connectorNodeV1} = require('@opuscapita/react-filemanager-connector-node-v1');
 
 const apiOptions = {
-    ...connectorNodeV1.apiOptions,
-    apiRoot: `http://localhost:3020` // Or you local Server Node V1 installation.
+  ...connectorNodeV1.apiOptions,
+  apiRoot: `http://localhost:3020` // Or you local Server Node V1 installation.
 };
+console.log(apiOptions, connectorNodeV1.apiOptions);
 
-const fileManager = createReactClass({
-    render: function() {
-        React.createElement('div', {
-            style: {
-                height: '480px'
-            }
-        }, React.createElement(FileManager, null, React.createElement(FileNavigator, {
-            id               : 'filemanager-1',
-            api              : connectorNodeV1.api,
-            apiOptions       : apiOptions,
-            capabilities     : connectorNodeV1.capabilities,
-            listViewLayout   : connectorNodeV1.listViewLayout,
-            viewLayoutOptions: connectorNodeV1.viewLayoutOptions
-        }), ' '), ' ');
+/* const fileManager = function() {
+  return React.createElement('div', {
+    style: {
+      height: '480px'
     }
-});
+  }, React.createElement(FileManager, null, React.createElement(FileNavigator, {
+    id               : 'filemanager-1',
+    api              : connectorNodeV1.api,
+    apiOptions       : apiOptions,
+    capabilities     : connectorNodeV1.capabilities,
+    listViewLayout   : connectorNodeV1.listViewLayout,
+    viewLayoutOptions: connectorNodeV1.viewLayoutOptions
+  }), ' '), ' ');
+} */
+
+
+class fileManager extends React.Component {
+  divStyle = {
+    height: 100 + '%'
+  };
+  render() {
+    return html
+    `<div style=${this.divStyle}>
+    <${FileManager}>
+        <${FileNavigator}
+        id="filemanager-1"
+        api=${connectorNodeV1.api}
+        apiOptions=${apiOptions}
+        capabilities=${connectorNodeV1.capabilities}
+        listViewLayout=${connectorNodeV1.listViewLayout}
+        viewLayoutOptions=${connectorNodeV1.viewLayoutOptions} />
+     <//>
+     <//>`
+  };
+}
+
+// ReactDOM.render(React.createElement(fileManager), document.querySelector('#mainLayout'))
+
 
 mainLayout.registerComponent('fileManager', fileManager);
-addMenuItem('FM', 'fileManager', {});
+addMenuItem('FM', 'fileManager', {}, "react-component");
--- renderer.js
+++ renderer.js
@@ -1,35 +1,38 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
 const config = require("./configs/config");
 
 const GoldenLayout = require("golden-layout");
 let mainLayout;
 
-const addMenuItem = (title, componentName, state = "") => {
+const addMenuItem = (title, componentName, state = "", type = "component") => {
   const element = $("<li>" + title + "</li>");
   $("#menuContainer ul").append(element);
   const newItemConfig = {
     title         : title,
-    type          : "component",
+    type          : type,
     componentName : componentName,
-    componentState: state
+    componentState: state,
+    component     : type = "react-component"
+      ? componentName
+      : undefined
+
   };
 
   mainLayout.createDragSource(element, newItemConfig);
 };
 
 document.addEventListener("DOMContentLoaded", async ev => {
-  mainLayout = new GoldenLayout(
-    require("./configs/mainLayout"),
-    document.querySelector("#mainLayout")
-  );
+  mainLayout = new GoldenLayout(require("./configs/mainLayout"), document.querySelector("#mainLayout"));
   require("./components/xterm");
   require("./components/monaco");
   require("./components/palette");
+  require("./components/filemanager");
   require('./common/keyboard');
   window.addEventListener("load", e => {
     mainLayout.init();
 
-// mainLayout.on('stateChanged', ev => console.log(ev));
-// mainLayout.on('selectionChanged', ev => console.log(ev));
+// mainLayout.on('stateChanged', ev => console.log(ev)); mainLayout.on('selectionChanged', ev => console.log(ev));
     mainLayout.on("stackCreated", ev => {
       console.log(ev);
 
